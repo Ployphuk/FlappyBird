1d289b74057d8c70e4bf3a5a85cc14433e3c8358
Natthaya-Polp <natthayacud56@gmail.com>
2023-08-30 19:27:19 +0700

    ADD: Animated bird

--- FlappyBird/Assets/Scripts/Player.cs
+++ FlappyBird/Assets/Scripts/Player.cs
@@ -4,17 +4,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
+    public Sprite[] sprites;
+    private int spriteIndex;
+    
     private Vector3 direction;
+
     [SerializeField] public float gravity = -9.8f;
     [SerializeField] float strength = 5f;
-     // Added variable to track game start
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
+    }
+
     private void OnEnable(){
         Vector3 position = transform.position;
         position.y= 0f;
         transform.position = position;
         direction = Vector3.zero;
     }
-    private void Update(){
+
+    private void Update()
+    {
         if(Input.GetKeyDown(KeyCode.Space) ||  Input.GetMouseButtonDown(0)){
             direction = Vector3.up * strength;
         }
@@ -29,5 +46,17 @@ public class Player : MonoBehaviour
         
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
+    }
+
+    private void AnimateSprite()
+    {
+        spriteIndex++;
+
+        if (spriteIndex >= sprites.Length)
+        {
+            spriteIndex = 0;
+        }
+
+        spriteRenderer.sprite = sprites[spriteIndex];
     }
 }
